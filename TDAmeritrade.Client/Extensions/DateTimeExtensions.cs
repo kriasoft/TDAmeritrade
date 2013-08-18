394022ed32dbb1a30d68258012b03560220d9d34
Konstantin Tarkus <hello@tarkus.me>
2013-08-18 17:22:37 +0400

    Add DateTime.ToServerTime() extension method

---
+++ TDAmeritrade.Client/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DateTimeExtensions.cs" company="KriaSoft LLC">
+//   Copyright © 2013 Konstantin Tarkus, KriaSoft LLC. See LICENSE.txt
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TDAmeritrade.Client.Extensions
+{
+    using System;
+
+    public class DateTimeExtensions
+    {
+        private static readonly TimeZoneInfo ServerTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+
+        public static DateTime ToServerTime(this DateTime dateTime)
+        {
+            return TimeZoneInfo.ConvertTime(dateTime, ServerTimeZone);
+        }
+    }
+}
